cf10bbe6499117e86d3fc525dce78930f68bbfb4
Alexander.Sabanadze <alexander.sabanadze@ol.ge>
2020-03-25 17:25:01 +0400

    Add project files.

---
+++ FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult SayHi()
+        {
+            return View(new SayHiViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult SayHi(SayHiViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Hi", model);
+            }
+
+            return View(model);
+        }
+
+
+        public ActionResult Hi(HiViewModel model)
+        {
+            if (model == null)
+            {
+                model = new HiViewModel();
+            }
+            model.GreetingSuffix = DateTime.Now.Hour > 12 ? "Evening" : "Morning";
+
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FirstApp/Models/HiViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Models
+{
+    public class HiViewModel
+    {
+        public String Name { get; set; }
+        public string GreetingSuffix { get; set; }
+    }
+}
